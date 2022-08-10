b5bc6188f9eb2e2c77be0454efd65330b84c8135
Raheem Abolfathzadeh <raheemabol91@gmail.com>
2022-08-10 19:01:49 -0400

    created findall method and impl in studentserviceimpl

--- studentsystem/src/main/java/com/abol/studentsystem/service/StudentService.java
+++ studentsystem/src/main/java/com/abol/studentsystem/service/StudentService.java
@@ -2,7 +2,10 @@ package com.abol.studentsystem.service;
 
 import com.abol.studentsystem.model.Student;
 
+import java.util.List;
+
 public interface StudentService {
 
     public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
 }
